4e45ca0909eac17fef211cf2556f30cf692a11aa
chetong <chetong@maoa.com>
2019-08-05 18:11:29 +0800

    测试

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+require_once "./vendor/autoload.php";
+
+use Hello\World;
+
+$world=new World();
+$world->text();
---
+++ src/Hello/World.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hello;
+
+/**
+ * Created by PhpStorm.
+ * @Author: Tong
+ * @Date: 2019/7/19 14:51
+ */
+class World
+{
+    public function text()
+    {
+        echo 'hellow World';
+    }
+}
